3df83f9805a13d75a17476cd4049551d374a6579
JeffCheng <jeff97830133@gmail.com>
2016-04-01 21:05:19 +0800

    db.sqlite3

---
+++ restaurants/views.py
@@ -0,0 +1,11 @@
+﻿# -*- coding: utf-8 -*-
+from django.shortcuts import render_to_response
+from django import template
+from django.http import HttpResponse
+
+def menu(request):
+	food = {'name':'tomato', 'price':60, 'comment':'goodtoeat', 'is_spicy':False }
+	
+	food2 = {'name':'pea', 'price': 80, 'comment' : 'notbad', 'is_spicy':True}
+	foods = [food,food2]
+	return render_to_response('menu.html', locals())
